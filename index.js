5cd9993de9d3680743bfde035e77c3990ca8ea2f
ketchanji <valerydjanya8@gmail.com>
2022-11-04 03:05:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// const adviceText = document.querySelector(".body .numero"),
+// adviceId = document.querySelector(".heading span"),
+// adviceBtn = document.querySelector(".dice");
+// console.log("adviceBtn");
+
+
+// function randomAdvice(){
+//     quoteBtn.innerText = "Loading Advice....";
+//     fetch("https://api.adviceslip.com").then(res => res.json()).then(result =>{
+//         quoteText.innerText = result.content;
+//         authorName.innerText = result.author; 
+//    });
+
+// }
